48fa6f2bcef6c5265a30f49ee4b25bb9bf9e0213
TwistzTitan <leozinhuhonorio@gmail.com>
2019-10-18 12:22:34 -0300

    Adjust master

--- iblue/server/credentials.js
+++
@@ -1,9 +0,0 @@
-const credentials = { 
-    login:"planet_app",
-    pass:"iblueplanet",
-    db:'test'
-}
-
-const url = "mongodb+srv://"+credentials.login+":"+credentials.pass+"@tw-mgdb-001-3snfd.mongodb.net/test?retryWrites=true&w=majority"
-
-module.exports = url 
